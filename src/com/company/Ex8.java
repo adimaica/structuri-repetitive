f3e944c9a66bbf59bec350bf0b95c042a713a780
adimaica <adimaica16@gmail.com>
2019-05-06 02:49:32 +0300

    Pow

---
+++ src/com/company/Ex8.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Ex8 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        System.out.println(Math.pow(a,b));
+    }
+}
