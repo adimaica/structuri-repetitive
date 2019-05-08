03fb0af373fa60aca36f801f579d6364018669b1
adimaica <adimaica16@gmail.com>
2019-05-08 21:35:50 +0300

    Initial Commit

--- src/com/company/Ex10.java
+++ src/com/company/Ex10.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class Ex10 {
+    public static void main(String[] args) {
+        int[] sirNumere = {2, 14, 5, 7, 10, 12, 6, 8, 24, 15, 20};
+        int suma = 0;
+        int j=0;
+        for(int i=0; i< sirNumere.length; i++)
+        {
+            if(sirNumere[i]%2 == 0 && j<5)
+            {
+                j++;
+                suma = suma + sirNumere[i];
+            }
+        }
+        System.out.println(suma);
+    }
 }
